2e418b816b8738eee418b306cf82d07ee0fd0e48
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-02-09 13:25:49 -0300

    Tentativa de criar um return std::make_tuple(x_list, list_sin);

--- dll/MCE.cpp
+++ dll/MCE.cpp
@@ -1,35 +1,26 @@
 #include <iostream>
 #include <cmath>
+#include <tuple>
 #include "MCE.h"
 
 #define PI 3.14159265358979323
 
 // void Animacao(char Hz) {}
 
-void PlotSin(float Hz) {
-	if (Hz == NULL) {
+std::tuple<float*, float*> PlotSin(float Hz) {
+	if (Hz == 0.0f) {
 		Hz = 1.0f;
-		float rad = 0.0f, x_total = 0.0f;
-		x_total = Hz * 360;
-		float * x_list = new float[x_total];
-		float* list_sin = new float[x_total];
-		for (int x = 0; x < x_total; x++) {
-			x_list[x] = x;
-			rad = (x * PI) / 180;
-			list_sin[x] = std::sin(rad);
-		}
-	}
-	else {
-		float rad = 0.0f, x_total = 0.0f;
-		x_total = Hz * 360;
-		float* x_list = new float[x_total];
-		float* list_sin = new float[x_total];
-		for (int x = 0; x < x_total; x++) {
-			x_list[x] = x;
-			rad = (x * PI) / 180;
-			list_sin[x] = std::sin(rad);
-		}
-	}
+	};
+	float rad = 0.0f, x_total = 0.0f;
+	x_total = Hz * 360;
+	float* x_list = new float[x_total];
+	float* list_sin = new float[x_total];
+	for (int x = 0; x < x_total; x++) {
+		x_list[x] = x;
+		rad = (x * PI) / 180;
+		list_sin[x] = std::sin(rad);
+	};
+	return std::make_tuple(x_list, list_sin);
 }
 
 // void PlotCic(char complex) {}
