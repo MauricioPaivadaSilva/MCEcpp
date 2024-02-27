a57a6222119defe56cf311cafe08351611c4c463
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-02-27 18:00:14 -0300

    Gerada a função senoidal.

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
 
 namespace mc{
   class principal{
     private:
       sf::RenderWindow window;
+      auto position();
     public:
       principal();
       ~principal();
@@ -13,5 +15,6 @@ namespace mc{
       auto GraphFunc();
       auto Vector();
       auto Arrow();
+      auto funcSin();
   };
 }
--- src/graph.cpp
+++ src/graph.cpp
@@ -4,16 +4,66 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 #include "../include/graph.hpp"
+
+sf::VertexArray L_FUNC;
 
 mc::principal::principal(): window(sf::VideoMode::getDesktopMode(), "MCEcpp"){
   exec();
 }
 
+auto mc::principal::funcSin(){//Gerador dos dados para a criação do gráfico senoidal.
+  float Hz, cic, val_x;
+  Hz = 60.0f;
+  cic = Hz*360.0f;
+  float data[(int)cic];
+
+  for(int i = 0; i < (int)cic; i++){
+    float rad;
+    float y;
+    const float PI = 3.14f;
+    rad = ((float)i*PI)/180.0f;
+    y = std::sin(2.0f * PI * (rad/(1.0f/Hz)));
+    data[i] = y;
+  };
+
+
+  sf::VertexArray l_func(sf::LineStrip, (int)cic);
+/*  l_func[0].position = sf::Vector2f(15.0f, 15.0f);
+  l_func[0].color = sf::Color::Blue;
+  l_func[1].position = sf::Vector2f(20.0f, 100.0f);
+  l_func[1].color = sf::Color::Blue;
+  l_func[2].position = sf::Vector2f(30.0f, 100.0f);
+  */l_func[2].color = sf::Color::Blue;
+  val_x = 0.0f;
+
+  
+  for(int i = 0; i < (int)cic; i++){
+    l_func[i].position = sf::Vector2f(val_x, ((data[i] * 10.0f) + 100.0f));
+    l_func[i].color = sf::Color::Blue;
+    val_x += 1.0f;
+  };
+
+  L_FUNC = l_func;
+  return l_func;
+}
+
+auto mc::principal::position(){ //Função para gerar os dados da posição do gráfico.
+  sf::Vector2u window_size = mc::principal::principal::window.getSize();
+  float w_size_x = window_size.x;
+  float w_size_y = window_size.y;
+  float pos_x = (w_size_x/5);
+  float pos_y = (9.0f/10.0f) * (w_size_y);
+
+  std::vector<float> position = {pos_x, pos_y};
+  return position;
+}
+
 mc::principal::~principal(){}
 
-auto mc::principal::GraphFunc(){
+auto mc::principal::GraphFunc(){ //Cria  gráfico (layout)
 
   sf::Vector2u window_size = mc::principal::principal::window.getSize();
   float w_size_x = window_size.x;
@@ -39,7 +89,7 @@ auto mc::principal::GraphFunc(){
   return graph_func;
 }
 
-auto mc::principal::Vector(){
+auto mc::principal::Vector(){ //Cria o eixo "x" dentro do gráfico.
   sf::Vector2u window_size = mc::principal::principal::window.getSize();
   float w_size_x = window_size.x;
   float w_size_y = window_size.y;
@@ -56,7 +106,7 @@ auto mc::principal::Vector(){
   return vector;
 }
 
-auto mc::principal::Arrow(){
+auto mc::principal::Arrow(){ //Cira a ponta do eixo
 
   sf::Vector2u window_size = mc::principal::principal::window.getSize();
   float w_size_x = window_size.x;
@@ -76,7 +126,7 @@ auto mc::principal::Arrow(){
   return arrow;
 }
 
-void mc::principal::exec(){
+void mc::principal::exec(){ //Executa toda a sequência que postra as janelas.
 
   sf::Vector2u window_size = mc::principal::principal::window.getSize();
 
@@ -102,6 +152,7 @@ void mc::principal::exec(){
     window.draw(mc::principal::GraphFunc());
     window.draw(mc::principal::Vector());
     window.draw(mc::principal::Arrow());
+    window.draw(mc::principal::funcSin());
     window.display();
   }
 }
